074e8b87fabecbb48c87b648ffe91a4b6f5cad31
hdong <spdonghao@gmail.com>
2019-06-04 12:27:29 -0400

    a2

--- Assignment 2/a2.py
+++ Assignment 2/a2.py
@@ -1,53 +1,81 @@
 import csv
 import sys
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn.pipeline import Pipeline
 import numpy as np
 
-def save_into_list(f, unigrams, bigrams, pos_or_neg, target):
+def save_into_list(f, data, pos_or_neg, target):
+
     for line in f.readlines():
+        line_str = ""
         line = line[1:-2].split(r", ")
         for i in range(len(line)):
             line[i] = line[i][1:-1]
-            unigrams.append(line[i])
-            target.append(pos_or_neg)
-        bigrams.extend([i for i in zip(line[:-1],line[1:])])
+            line_str += str(line[i]+" ")
+        data.append(line_str)
+        target.append(pos_or_neg)
+
+def classify(nr, train_data,train_target,test_data,test_target,val_data,val_target):
+    print(nr)
+    vectorizer = CountVectorizer(ngram_range = nr)
+    train_data = vectorizer.fit_transform(train_data)
+    val_data = vectorizer.transform(val_data)
+    test_data = vectorizer.transform(test_data)
+
+    max_accu = 0
+    max_alpha = 0
+    for alpha in np.arange(0.1, 3, .1):
+        nbc = MultinomialNB(alpha = alpha)
+        nbc.fit(train_data, train_target) 
+        predict = nbc.predict(val_data) 
+        count = 0
+        for left , right in zip(predict, val_target):
+            if left == right:
+                count += 1
+        if(count/len(val_target) > max_accu):
+            max_accu = count/len(val_target)
+            max_alpha = alpha
+    print(max_accu)
+
+    nbc = MultinomialNB(alpha = max_alpha)
+    nbc.fit(train_data, train_target) 
+    predict = nbc.predict(test_data) 
+    count = 0
+    for left , right in zip(predict, test_target):
+        if left == right:
+            count += 1
+    print(count/len(test_target))
+
 
 if __name__ == "__main__":
 
-    train_unigrams = []
-    train_bigrams = []
+    train_data = []
     train_target = []
-    val_unigrams = []
-    val_bigrams = []
+    val_data = []
     val_target = []
-    test_unigrams = []
-    test_bigrams = []
+    test_data = []
     test_target = []
 
     for i in range(1,len(sys.argv)):
         f = open(sys.argv[i],'r') 
-        feature_type = sys.argv[i][:2]
-        pos_or_neg = 1 if sys.argv[i][-7:-4] == 'pos' else 0
+        file_name = sys.argv[i]
+        pos_or_neg = 1 if 'pos' in file_name else 0
 
-        if( feature_type == 'tr'):
-            save_into_list(f, train_unigrams, train_bigrams, pos_or_neg, train_target)
-        elif( feature_type == 'te'):
-            save_into_list(f, test_unigrams, test_bigrams, pos_or_neg, test_target)
+        if( "train" in file_name):
+            save_into_list(f, train_data, pos_or_neg, train_target)
+        elif( "test" in file_name):
+            save_into_list(f, test_data, pos_or_neg, test_target)
         else:
-            save_into_list(f, val_unigrams, val_bigrams, pos_or_neg, val_target)
+            save_into_list(f, val_data, pos_or_neg, val_target)
         f.close()
+    print("finish processing")
     
-    for alpha in range(1,20):
-        nbc = Pipeline([
-        ('vect', TfidfVectorizer( )),
-        ('clf', MultinomialNB(alpha = alpha))])
-        nbc.fit(train_unigrams, train_target) 
-        predict = nbc.predict(test_unigrams) 
-        count = 0
-        for left , right in zip(predict, test_target):
-            if left == right:
-                count += 1
-        print(str(alpha)+":")
-        print(count/len(test_target))
+
+
+    ngram_range = [(1,1),(2,2),(1,2)]
+    for nr in ngram_range:
+        classify(nr, train_data,train_target,test_data,test_target,val_data,val_target)
+
+    
+
